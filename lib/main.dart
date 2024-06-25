a13117f96151603efe6214159792e60cf49e009e
vkrpk <victor.krupka@orange.fr>
2024-06-25 15:35:01 +0200

    Implementation of Slot Machine game in Flutter with random icon display, win/loss logic, and special jackpot message.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+List<String> images = [
+  'images/bar.png',
+  'images/cerise.png',
+  'images/cloche.png',
+  'images/diamant.png',
+  'images/fer-a-cheval.png',
+  'images/pasteque.png',
+  'images/sept.png',
+];
+
+final _random = Random();
 
 void main() {
   runApp(const MyApp());
@@ -29,9 +43,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _message = '';
+
+  void play () {
+    setCurrentImages();
+    bool everyValueIsEmpty = currentImages.every((item) => item.image.toString() == currentImages[0].image.toString());
+    setMessage(everyValueIsEmpty);
+  }
+
+  List<Image> currentImages = [
+    Image.asset(images[_random.nextInt(images.length)]),
+    Image.asset(images[_random.nextInt(images.length)]),
+    Image.asset(images[_random.nextInt(images.length)]),
+  ];
+
+  void setMessage(state) {
+    setState(() {
+      _message = state ? 'Jackpot' : 'You lost, try again !';
+    });
+  }
+
+  void setCurrentImages() {
+    setState(() {
+      currentImages[0] = Image.asset(images[_random.nextInt(images.length)]);
+      currentImages[1] = Image.asset(images[_random.nextInt(images.length)]);
+      currentImages[2] = Image.asset(images[_random.nextInt(images.length)]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -41,8 +82,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: currentImages,
+            ),
+            Text(_message, style: const TextStyle(height: 5, fontSize: 50), )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton.large(
+          onPressed: () => play(),
+          child: const Icon(Icons.autorenew)
       ),
     );
   }
